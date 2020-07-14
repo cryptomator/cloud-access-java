20a33fb57b689a2bd6e4d9615859d83f455940f6
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2020-07-14 09:15:57 +0200

    Added public API (in exported package)

--- src/main/java/org/cryptomator/cloudaccess/CloudAccess.java
+++ src/main/java/org/cryptomator/cloudaccess/CloudAccess.java
@@ -1,13 +1,29 @@
 package org.cryptomator.cloudaccess;
 
+import java.net.URL;
 import java.nio.file.Path;
 
 import org.cryptomator.cloudaccess.api.CloudProvider;
 import org.cryptomator.cloudaccess.localfs.LocalFsCloudProvider;
+import org.cryptomator.cloudaccess.webdav.WebDavCloudProvider;
+import org.cryptomator.cloudaccess.webdav.WebDavCredential;
 
 public class CloudAccess {
 
 	private CloudAccess() {
+	}
+
+	/**
+	 * Creates a new CloudProvider which provides access to the given URL via WebDAV.
+	 *
+	 * @param url Base URL leading to the root resource
+	 * @param username Username used during basic or digest auth challenges
+	 * @param password Password used during basic or digest auth challenges
+	 * @return A cloud access provider that provides access to the given WebDAV URL.
+	 */
+	public static CloudProvider toWebDAV(URL url, String username, CharSequence password) {
+		// TODO adapt types
+		return WebDavCloudProvider.from(WebDavCredential.from(null, username, null));
 	}
 
 	/**
