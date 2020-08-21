fa01c9f83e28ed15b8184bc691b3f05b5eccaaff
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2020-08-21 12:50:39 +0200

    Added integration test template

---
+++ src/test/java/org/cryptomator/cloudaccess/vaultformat8/VaultFormat8IntegrationTest.java
@@ -0,0 +1,48 @@
+package org.cryptomator.cloudaccess.vaultformat8;
+
+import org.cryptomator.cloudaccess.CloudAccess;
+import org.cryptomator.cloudaccess.api.CloudPath;
+import org.cryptomator.cloudaccess.api.CloudProvider;
+import org.cryptomator.cloudaccess.api.ProgressListener;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.Random;
+
+public class VaultFormat8IntegrationTest {
+
+	private static final Duration TIMEOUT = Duration.ofMillis(100);
+
+	private CloudProvider localProvider;
+	private CloudProvider encryptedProvider;
+
+	@BeforeEach
+	public void setup(@TempDir Path tmpDir) {
+		this.localProvider = CloudAccess.toLocalFileSystem(tmpDir);
+		this.encryptedProvider = CloudAccess.vaultFormat8GCMCloudAccess(localProvider, CloudPath.of("/"), new byte[64]);
+	}
+
+	@Test
+	public void writeThenReadFile() throws IOException {
+		var path = CloudPath.of("/file.txt");
+		var content = new byte[100_000];
+		new Random(42l).nextBytes(content);
+
+		// write 100k
+		var futureMetadata = encryptedProvider.write(path, true, new ByteArrayInputStream(content), ProgressListener.NO_PROGRESS_AWARE);
+		var metadata = Assertions.assertTimeoutPreemptively(TIMEOUT, () -> futureMetadata.toCompletableFuture().get());
+		Assertions.assertEquals(content.length, metadata.getSize().get());
+
+		// read all bytes
+		var futureInputStream = encryptedProvider.read(path, ProgressListener.NO_PROGRESS_AWARE);
+		var inputStream = Assertions.assertTimeoutPreemptively(TIMEOUT, () -> futureInputStream.toCompletableFuture().get());
+		Assertions.assertArrayEquals(content, inputStream.readAllBytes());
+	}
+
+}
