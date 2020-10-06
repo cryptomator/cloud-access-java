b4fb868d3cb6466f40f4c24eebed3ea296883923
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2020-10-06 10:42:16 +0200

    Added new CloudTimeoutException

---
+++ src/main/java/org/cryptomator/cloudaccess/api/exceptions/CloudTimeoutException.java
@@ -0,0 +1,9 @@
+package org.cryptomator.cloudaccess.api.exceptions;
+
+public class CloudTimeoutException extends CloudProviderException {
+
+	public CloudTimeoutException(Throwable cause) {
+		super(cause);
+	}
+
+}
